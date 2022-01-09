45bd8fc9099494f32cf19f6fb80ed03a0e77686f
Haashim Ameer <34925045+HaashimAmeer@users.noreply.github.com>
2022-01-08 18:30:56 -0800

    PUSH new post to server

--- index.js
+++ index.js
@@ -12,5 +12,30 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
       `;
     }
     document.getElementById("root").innerHTML = html;
-    console.log(postsArr);
+    // console.log(postsArr);
+  });
+
+document
+  .getElementById("new-post")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const postTitle = document.getElementById("post-title").value;
+    const postBody = document.getElementById("post-body").value;
+
+    let post = {
+      title: postTitle,
+      body: postBody,
+    };
+
+    console.log(post);
+
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
+      method: "POST",
+      body: JSON.stringify(post),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
   });
